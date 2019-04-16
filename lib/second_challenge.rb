a76a9ec2071e91c5729777c44bc580341a65e2bc
Francis Mendez <fmendez884@gmail.com>
2019-04-16 23:10:40 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,21 +2,11 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-      :patriarch => {
-        :name => "Lord Montague",
-        :age => "53"
-      },
-      :matriarch => {
-        :name => "Lady Montague",
-        :age => "54"
-      },
-      :hero => {
-        :name => "Romeo",
-        :age => "15"
-      },
-      :hero_friends => [
-        ]
-      }, 
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
     :capulet => {
       #your key/value pairs here!
       :patriarch => {},
