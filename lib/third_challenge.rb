e7bec2ad6ba5fb62b9f88e6a53783728a98e6c54
Francis Mendez <fmendez884@gmail.com>
2019-04-16 23:12:54 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -12,7 +12,8 @@ def third_challenge
         
       },
       :hero => {
-        name: "Romeo", age: "15", status: "alive"},
+        name: "Romeo",
+        age: "15", status: "alive"},
       :hero_friends => []
    },
    :capulet => {
