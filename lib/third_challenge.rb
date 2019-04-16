ba86ba4fc852d06b942628e83067c78540559ea4
Francis Mendez <fmendez884@gmail.com>
2019-04-16 23:13:28 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -4,7 +4,6 @@ def third_challenge
       :patriarch => {
         name: "Lord Montague", 
         age: "53"
-        
       },
       :matriarch => {
         name: "Lady Montague",
@@ -18,7 +17,8 @@ def third_challenge
       :hero_friends => []
    },
    :capulet => {
-      :patriarch => {name: "Lord Capulet", age: "50"},
+      :patriarch => {
+        name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => []
